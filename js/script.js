aa8f535d92f63b6a6e35c12c24ea35d12c4aa19d
coder-rakib-62 <abdulmazidrakib908@gmail.com>
2026-04-27 23:07:04 +0600

    main

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
-$('.counter').counterUp({
-    delay: 10,
-    time: 1000
+$(document).ready(function () {
+  $(".testimonial_slide").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true, 
+    dots: true,
+    prevArrow:'<i class="fa-solid fa-circle-arrow-left"></i>',
+    nextArrow:'<i class="fa-solid fa-circle-arrow-right"></i>'
+
+  });
 });
