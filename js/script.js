b3f3480f4b22f253ce589b8dbc7658b7eaae2e5c
coder-rakib-62 <abdulmazidrakib908@gmail.com>
2026-04-29 22:13:54 +0600

    push

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     slidesToShow: 1,
     slidesToScroll: 1,
     infinite: true, 
-    dots: true,
+    // dots: true,
     prevArrow:'<i class="fa-solid fa-circle-arrow-left"></i>',
     nextArrow:'<i class="fa-solid fa-circle-arrow-right"></i>'
 
