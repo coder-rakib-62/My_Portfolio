bf5daf65ba5151e345d9adcad92a6cb8e2658dfc
coder-rakib-62 <abdulmazidrakib908@gmail.com>
2026-05-12 22:20:04 +0600

    push

--- js/script.js
+++ js/script.js
@@ -1,11 +1,32 @@
-$(document).ready(function () {
-  $(".testimonial_slide").slick({
+// $(document).ready(function () {
+//   $(".testimonial_slide").slick({
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     infinite: true, 
+//     // dots: true,
+//     prevArrow:'<i class="fa-solid fa-circle-arrow-left"></i>',
+//     nextArrow:'<i class="fa-solid fa-circle-arrow-right"></i>'
+
+//   });
+// });
+  $('.testimonial_slider').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
-    infinite: true, 
-    // dots: true,
-    prevArrow:'<i class="fa-solid fa-circle-arrow-left"></i>',
-    nextArrow:'<i class="fa-solid fa-circle-arrow-right"></i>'
+    infinite: true,
+    autoplay: false,
+    arrows: true,
+    dots: false,
+    speed: 700,
 
+    prevArrow: `
+      <button type="button" class="slick-prev custom_arrow">
+        <i class="fa-solid fa-arrow-left"></i>
+      </button>
+    `,
+
+    nextArrow: `
+      <button type="button" class="slick-next custom_arrow">
+        <i class="fa-solid fa-arrow-right"></i>
+      </button>
+    `
   });
-});
